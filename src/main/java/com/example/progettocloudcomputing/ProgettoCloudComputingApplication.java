aed30896caaaf33aebbea0f70a4c277fc6ff6b6d
andreasasso9 <andry-sasso@outlook.it>
2025-02-26 09:25:20 +0100

    creazione progetto

---
+++ src/main/java/com/example/progettocloudcomputing/ProgettoCloudComputingApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettocloudcomputing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoCloudComputingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoCloudComputingApplication.class, args);
+	}
+
+}
