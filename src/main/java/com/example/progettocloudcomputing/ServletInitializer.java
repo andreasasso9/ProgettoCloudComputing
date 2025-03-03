67e6b4040e0cfbe44a98e395b1c8c887a187186e
andreasasso9 <andry-sasso@outlook.it>
2025-03-03 18:48:48 +0100

    creazione progetto

---
+++ src/main/java/com/example/progettocloudcomputing/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.progettocloudcomputing;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProgettoCloudComputingApplication.class);
+	}
+
+}
---
+++ src/test/java/com/example/progettocloudcomputing/ProgettoCloudComputingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progettocloudcomputing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoCloudComputingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
