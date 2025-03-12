bc850836c724775abe7d85a1b9fecf9d8687293a
andreasasso9 <andry-sasso@outlook.it>
2025-03-12 12:07:19 +0100

    aggiunta security

--- src/main/java/com/example/progettocloudcomputing/SecurityConfig.java
+++ src/main/java/com/example/progettocloudcomputing/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,7 +15,7 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.formLogin(Customizer.withDefaults())
+		http.formLogin(AbstractHttpConfigurer::disable)
 				.logout(logout->SecurityContextHolder.clearContext())
 				.authorizeHttpRequests(request->request.requestMatchers("/").authenticated().anyRequest().permitAll());
 		return http.build();
