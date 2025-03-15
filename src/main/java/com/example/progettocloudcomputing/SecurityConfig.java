60301f6ef3c3331eeba7ff45c73fc057f6cfac04
andreasasso9 <andry-sasso@outlook.it>
2025-03-15 16:24:07 +0100

    prova security

--- src/main/java/com/example/progettocloudcomputing/SecurityConfig.java
+++ src/main/java/com/example/progettocloudcomputing/SecurityConfig.java
@@ -2,10 +2,8 @@ package com.example.progettocloudcomputing;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,7 +14,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-				.authorizeHttpRequests(request->request.requestMatchers("/", "/login", "/logut", "/.auth/me", "/user/save", "/style/*").permitAll().anyRequest().authenticated())
+				.authorizeHttpRequests(request->request.requestMatchers("/", "/login", "/logut", "/.auth/me", "/user/save", "/style/*", "/index").permitAll().anyRequest().authenticated())
 				.oauth2Login(login->login.loginPage("/login.html").permitAll())
 				.logout(LogoutConfigurer::permitAll);
 
