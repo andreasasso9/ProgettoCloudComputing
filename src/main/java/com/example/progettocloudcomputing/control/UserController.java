408c3580d84b6e6ac12b8a5525bec23013c63fbf
andreasasso9 <andry-sasso@outlook.it>
2025-03-15 17:14:49 +0100

    prova security

--- src/main/java/com/example/progettocloudcomputing/control/UserController.java
+++ src/main/java/com/example/progettocloudcomputing/control/UserController.java
@@ -1,7 +1,11 @@
 package com.example.progettocloudcomputing.control;
 
+import com.example.progettocloudcomputing.entity.User;
 import com.example.progettocloudcomputing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,13 +20,13 @@ public class UserController {
 		this.userService = userService;
 	}
 
-//	@GetMapping("/save")
-//	public String save(@AuthenticationPrincipal OidcUser user) {
-//		User u=new User(user);
-//
-////		boolean result=userService.save(u);
-////		System.out.println(result);
-//
-//		return u.toString();
-//	}
+	@GetMapping("/save")
+	public String save(@AuthenticationPrincipal OidcUser user) {
+		User u=new User(user);
+
+//		boolean result=userService.save(u);
+//		System.out.println(result);
+
+		return u.toString();
+	}
 }
