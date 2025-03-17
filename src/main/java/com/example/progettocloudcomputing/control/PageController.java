2313329ce3feba9ec1910549f20d124d82a6b0fe
andreasasso9 <andry-sasso@outlook.it>
2025-03-17 19:51:20 +0100

    aggiornato login

--- src/main/java/com/example/progettocloudcomputing/control/PageController.java
+++ src/main/java/com/example/progettocloudcomputing/control/PageController.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,26 +25,10 @@ public class PageController {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		User u=new User();
 
-		if (authentication instanceof DefaultOAuth2User) {
-			DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
-			u.setEmail(user.getAttribute("email"));
-			u.setName(user.getAttribute("name"));
-			userService.save(u);
+		DefaultOAuth2User user=(DefaultOAuth2User) authentication.getPrincipal();
 
-			model.addAttribute("login", "default");
-		} else if (authentication instanceof OAuth2User) {
-			OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
-			u.setEmail(oauth2User.getAttribute("email"));
-			u.setName(oauth2User.getAttribute("name"));
-			userService.save(u);
-
-			model.addAttribute("login", "user");
-		} else {
-			u.setEmail("anonymous");
-			u.setName("anonymous");
-
-			model.addAttribute("login", "anonymous");
-		}
+		u.setName(user.getName());
+		u.setEmail(user.getAttribute("email"));
 
 		model.addAttribute("user", u);
 
--- src/main/java/com/example/progettocloudcomputing/control/UserController.java
+++ src/main/java/com/example/progettocloudcomputing/control/UserController.java
@@ -1,13 +1,9 @@
 package com.example.progettocloudcomputing.control;
 
-import com.example.progettocloudcomputing.entity.User;
 import com.example.progettocloudcomputing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +22,7 @@ public class UserController {
 	@GetMapping("/save")
 	public String save() {
 		Authentication auth=SecurityContextHolder.getContext().getAuthentication();
-		return String.format("credentials: %s\nprincipal: %s\nname: %s\ndetails: %s", auth.getCredentials(), auth.getPrincipal(), auth.getName(), auth.getDetails());
+		return String.format("credentials: %s<br>principal: %s<br>name: %s<br>details: %s", auth.getCredentials(), auth.getPrincipal(), auth.getName(), auth.getDetails());
 //		User user = new User();
 //		if (user!=null) {
 //			User u = new User(user);
