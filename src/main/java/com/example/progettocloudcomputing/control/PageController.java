32f8250863bb2a3ba5c7c57de6c57ad9a3bf867a
andreasasso9 <andry-sasso@outlook.it>
2025-03-17 19:14:25 +0100

    aggiornato login

--- src/main/java/com/example/progettocloudcomputing/control/PageController.java
+++ src/main/java/com/example/progettocloudcomputing/control/PageController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,10 +28,13 @@ public class PageController {
 
 		if (authentication instanceof DefaultOAuth2User) {
 			DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
-
 			u.setEmail(user.getAttribute("email"));
 			u.setName(user.getAttribute("name"));
-
+			userService.save(u);
+		} else if (authentication instanceof OAuth2User) {
+			OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+			u.setEmail(oauth2User.getAttribute("email"));
+			u.setName(oauth2User.getAttribute("name"));
 			userService.save(u);
 		} else {
 			u.setEmail("anonymous");
